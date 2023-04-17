veliu/pim-community-dev

PIM-10909: Refactor command to remove non-existing products and models from ES index

src/Akeneo/Pim/Enrichment/Bundle/Command/CleanRemovedProductsCommand.php
@@ -2,16 +2,12 @@
 
 namespace Akeneo\Pim\Enrichment\Bundle\Command;
 
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Indexer\ProductAndAncestorsIndexer;
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Model\ElasticsearchProductProjection;
-use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
+use Akeneo\Pim\Enrichment\Bundle\Storage\ElasticsearchAndSql\ProductAndProductModel\GetDeletedProductDocumentIds;
+use Akeneo\Pim\Enrichment\Bundle\Storage\ElasticsearchAndSql\ProductAndProductModel\GetDeletedProductModelDocumentIds;
 use Akeneo\Tool\Bundle\ElasticsearchBundle\Client;
-use Doctrine\DBAL\Connection;
-use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -23,170 +19,60 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class CleanRemovedProductsCommand extends Command
 {
-    private const DEFAULT_BATCH_SIZE = 1000;
+    private const DEFAULT_BATCH_SIZE = 100;
 
     protected static $defaultName = 'pim:product:clean-removed-products';
-    protected static $description = 'Erase documents present in Elasticsearch but missing in MySQL';
+    protected static $defaultDescription = 'Erase documents present in Elasticsearch but missing in MySQL';
 
     public function __construct(
-        private ProductAndAncestorsIndexer $productAndAncestorsIndexer,
-        private Client $productAndProductModelClient,
-        private Connection $connection
+        private readonly GetDeletedProductDocumentIds $getDeletedProductDocumentIds,
+        private readonly GetDeletedProductModelDocumentIds $getDeletedProductModelDocumentIds,
+        private readonly Client $productAndProductModelClient,
     ) {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addOption(
-                'batch-size',
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Number of products to index per batch',
-                self::DEFAULT_BATCH_SIZE
-            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->checkIndexExists();
 
-        $batchSize = (int) $input->getOption('batch-size') ?: self::DEFAULT_BATCH_SIZE;
-
-        $numberOfIndexedProducts = $this->removeDocumentFromIndex($output, $batchSize) ?? 0;
-
+        $deletedProductModelCount = $this->deleteDocumentsFromIndex(($this->getDeletedProductModelDocumentIds)(), $output);
         $output->writeln('');
-        $output->writeln(sprintf('<info>%d products de-indexed</info>', $numberOfIndexedProducts));
+        $output->writeln(\sprintf('<info>%d product model documents removed from the index</info>', $deletedProductModelCount));
+
+        $deletedProductCount = $this->deleteDocumentsFromIndex(($this->getDeletedProductDocumentIds)(), $output);
+        $output->writeln('');
+        $output->writeln(\sprintf('<info>%d product documents removed from the index</info>', $deletedProductCount));
 
         return Command::SUCCESS;
     }
 
-    private function removeDocumentFromIndex(OutputInterface $output, int $batchSize): int
+    private function deleteDocumentsFromIndex(iterable $getDeletedDocumentIds, OutputInterface $output): int
     {
-        $indexedProductCount = 0;
+        $numberOfDeleteDocuments = 0;
+        $documentsIdsToRemove = [];
+
         $progressBar = new ProgressBar($output, 0);
         $progressBar->start();
 
-        $esProductsIdsChunk = $this->fetchAllProductsIdsFromEsByChunk($batchSize);
-        $chunkedNonExistentProductIds = $this->filterNonExistentProductInMySQL($esProductsIdsChunk);
-
-        foreach ($chunkedNonExistentProductIds as $nonExistentProductIds) {
-            $uuids = array_map(fn ($id) => Uuid::fromString(($id)), $nonExistentProductIds);
-            $ancestorCodes = $this->getAncestorsFromProductsIds($nonExistentProductIds);
-            $this->productAndAncestorsIndexer->removeFromProductUuidsAndReindexAncestors(
-                $uuids,
-                $ancestorCodes
-            );
-
-            $indexedProductCount += count($nonExistentProductIds);
-            $progressBar->advance(count($nonExistentProductIds));
+        foreach ($getDeletedDocumentIds as $id) {
+            $documentsIdsToRemove[] = $id;
+            if (\count($documentsIdsToRemove) >= self::DEFAULT_BATCH_SIZE) {
+                $this->productAndProductModelClient->bulkDelete($documentsIdsToRemove);
+                $numberOfDeleteDocuments += \count($documentsIdsToRemove);
+                $progressBar->advance(\count($documentsIdsToRemove));
+                $documentsIdsToRemove = [];
+            }
         }
 
         $progressBar->finish();
 
-        return $indexedProductCount;
-    }
-
-    private function filterNonExistentProductInMySQL(iterable $esProductsIdsChunk): iterable
-    {
-        $sql = <<< SQL
-SELECT BIN_TO_UUID(uuid) AS uuid
-FROM pim_catalog_product
-WHERE BIN_TO_UUID(uuid) IN (:esIdentifiers) 
-SQL;
-
-        foreach ($esProductsIdsChunk as $productIdsFromEs) {
-            if (empty($productIdsFromEs)) {
-                break;
-            }
-            $productIdsFromMysql = $this->connection->executeQuery(
-                $sql,
-                [
-                    'esIdentifiers' => $productIdsFromEs
-                ],
-                [
-                    'esIdentifiers' => Connection::PARAM_STR_ARRAY,
-                ]
-            )->fetchFirstColumn();
-
-            $nonExistentProductIdsInMysql = array_diff($productIdsFromEs, $productIdsFromMysql);
-            if (!empty($nonExistentProductIdsInMysql)) {
-                yield $nonExistentProductIdsInMysql;
-            }
+        if (\count($documentsIdsToRemove) > 0) {
+            $this->productAndProductModelClient->bulkDelete($documentsIdsToRemove);
+            $numberOfDeleteDocuments += \count($documentsIdsToRemove);
         }
-    }
 
-    private function fetchAllProductsIdsFromEsByChunk(int $batchSize): iterable
-    {
-        $searchAfter = null;
-        do {
-            $params = array_merge(
-                [
-                    'sort' => ['id' => 'asc'],
-                    'size' => $batchSize,
-                    '_source' => ['id'],
-                    'query' => [
-                        'constant_score' => [
-                            'filter' => [
-                                'bool' => [
-                                    'filter' => [
-                                        'term' => [
-                                            'document_type' => ProductInterface::class
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ],
-                $searchAfter ? [
-                    'search_after' => $searchAfter
-                ] : []
-            );
-            $results = $this->productAndProductModelClient->search($params);
-
-            $productsIds = array_map(function ($doc) {
-                return substr($doc['_source']['id'], strlen(ElasticsearchProductProjection::INDEX_PREFIX_ID));
-            }, $results['hits']['hits']);
-
-            yield $productsIds;
-
-            $resultsPage = $results['hits']['hits'];
-            $lastResult = end($resultsPage);
-            $searchAfter = $lastResult['sort'] ?? [];
-        } while (count($resultsPage)>0);
-    }
-
-    private function getAncestorsFromProductsIds(array $productIds): array
-    {
-        $sql = <<<SQL
-            SELECT parent_product_model.code
-            FROM pim_catalog_product product
-                JOIN pim_catalog_product_model product_model
-                    ON product_model.id = product.product_model_id
-                JOIN pim_catalog_product_model parent_product_model
-                    ON parent_product_model.id = product_model.id
-            WHERE product.id IN (:product_ids)
-            UNION DISTINCT
-            SELECT product_model.code
-            FROM pim_catalog_product product
-                JOIN pim_catalog_product_model product_model
-                    ON product_model.id = product.product_model_id
-            WHERE product.id IN (:product_ids)
-        SQL;
-
-        $results = $this->connection->executeQuery(
-            $sql,
-            [
-                'product_ids' => $productIds
-            ],
-            [
-                'product_ids' => Connection::PARAM_STR_ARRAY,
-            ]
-        )->fetchFirstColumn();
-
-        return $results;
+        return $numberOfDeleteDocuments;
     }
 
     private function checkIndexExists(): void

slongiaru-dmind/akeneo-pim-community-dev

src/Akeneo/Pim/Enrichment/Bundle/Command/CleanRemovedProductsCommand.php
@@ -2,16 +2,12 @@
 
 namespace Akeneo\Pim\Enrichment\Bundle\Command;
 
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Indexer\ProductAndAncestorsIndexer;
-use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\Model\ElasticsearchProductProjection;
-use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
+use Akeneo\Pim\Enrichment\Bundle\Storage\ElasticsearchAndSql\ProductAndProductModel\GetDeletedProductDocumentIds;
+use Akeneo\Pim\Enrichment\Bundle\Storage\ElasticsearchAndSql\ProductAndProductModel\GetDeletedProductModelDocumentIds;
 use Akeneo\Tool\Bundle\ElasticsearchBundle\Client;
-use Doctrine\DBAL\Connection;
-use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -23,170 +19,60 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class CleanRemovedProductsCommand extends Command
 {
-    private const DEFAULT_BATCH_SIZE = 1000;
+    private const DEFAULT_BATCH_SIZE = 100;
 
     protected static $defaultName = 'pim:product:clean-removed-products';
-    protected static $description = 'Erase documents present in Elasticsearch but missing in MySQL';
+    protected static $defaultDescription = 'Erase documents present in Elasticsearch but missing in MySQL';
 
     public function __construct(
-        private ProductAndAncestorsIndexer $productAndAncestorsIndexer,
-        private Client $productAndProductModelClient,
-        private Connection $connection
+        private readonly GetDeletedProductDocumentIds $getDeletedProductDocumentIds,
+        private readonly GetDeletedProductModelDocumentIds $getDeletedProductModelDocumentIds,
+        private readonly Client $productAndProductModelClient,
     ) {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addOption(
-                'batch-size',
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Number of products to index per batch',
-                self::DEFAULT_BATCH_SIZE
-            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->checkIndexExists();
 
-        $batchSize = (int) $input->getOption('batch-size') ?: self::DEFAULT_BATCH_SIZE;
-
-        $numberOfIndexedProducts = $this->removeDocumentFromIndex($output, $batchSize) ?? 0;
-
+        $deletedProductModelCount = $this->deleteDocumentsFromIndex(($this->getDeletedProductModelDocumentIds)(), $output);
         $output->writeln('');
-        $output->writeln(sprintf('<info>%d products de-indexed</info>', $numberOfIndexedProducts));
+        $output->writeln(\sprintf('<info>%d product model documents removed from the index</info>', $deletedProductModelCount));
+
+        $deletedProductCount = $this->deleteDocumentsFromIndex(($this->getDeletedProductDocumentIds)(), $output);
+        $output->writeln('');
+        $output->writeln(\sprintf('<info>%d product documents removed from the index</info>', $deletedProductCount));
 
         return Command::SUCCESS;
     }
 
-    private function removeDocumentFromIndex(OutputInterface $output, int $batchSize): int
+    private function deleteDocumentsFromIndex(iterable $getDeletedDocumentIds, OutputInterface $output): int
     {
-        $indexedProductCount = 0;
+        $numberOfDeleteDocuments = 0;
+        $documentIdsToRemove = [];
+
         $progressBar = new ProgressBar($output, 0);
         $progressBar->start();
 
-        $esProductsIdsChunk = $this->fetchAllProductsIdsFromEsByChunk($batchSize);
-        $chunkedNonExistentProductIds = $this->filterNonExistentProductInMySQL($esProductsIdsChunk);
-
-        foreach ($chunkedNonExistentProductIds as $nonExistentProductIds) {
-            $uuids = array_map(fn ($id) => Uuid::fromString(($id)), $nonExistentProductIds);
-            $ancestorCodes = $this->getAncestorsFromProductsIds($nonExistentProductIds);
-            $this->productAndAncestorsIndexer->removeFromProductUuidsAndReindexAncestors(
-                $uuids,
-                $ancestorCodes
-            );
-
-            $indexedProductCount += count($nonExistentProductIds);
-            $progressBar->advance(count($nonExistentProductIds));
+        foreach ($getDeletedDocumentIds as $id) {
+            $documentIdsToRemove[] = $id;
+            if (\count($documentIdsToRemove) >= self::DEFAULT_BATCH_SIZE) {
+                $this->productAndProductModelClient->bulkDelete($documentIdsToRemove);
+                $numberOfDeleteDocuments += \count($documentIdsToRemove);
+                $progressBar->advance(\count($documentIdsToRemove));
+                $documentIdsToRemove = [];
+            }
         }
 
         $progressBar->finish();
 
-        return $indexedProductCount;
-    }
-
-    private function filterNonExistentProductInMySQL(iterable $esProductsIdsChunk): iterable
-    {
-        $sql = <<< SQL
-SELECT BIN_TO_UUID(uuid) AS uuid
-FROM pim_catalog_product
-WHERE BIN_TO_UUID(uuid) IN (:esIdentifiers) 
-SQL;
-
-        foreach ($esProductsIdsChunk as $productIdsFromEs) {
-            if (empty($productIdsFromEs)) {
-                break;
-            }
-            $productIdsFromMysql = $this->connection->executeQuery(
-                $sql,
-                [
-                    'esIdentifiers' => $productIdsFromEs
-                ],
-                [
-                    'esIdentifiers' => Connection::PARAM_STR_ARRAY,
-                ]
-            )->fetchFirstColumn();
-
-            $nonExistentProductIdsInMysql = array_diff($productIdsFromEs, $productIdsFromMysql);
-            if (!empty($nonExistentProductIdsInMysql)) {
-                yield $nonExistentProductIdsInMysql;
-            }
+        if (\count($documentIdsToRemove) > 0) {
+            $this->productAndProductModelClient->bulkDelete($documentIdsToRemove);
+            $numberOfDeleteDocuments += \count($documentIdsToRemove);
         }
-    }
 
-    private function fetchAllProductsIdsFromEsByChunk(int $batchSize): iterable
-    {
-        $searchAfter = null;
-        do {
-            $params = array_merge(
-                [
-                    'sort' => ['id' => 'asc'],
-                    'size' => $batchSize,
-                    '_source' => ['id'],
-                    'query' => [
-                        'constant_score' => [
-                            'filter' => [
-                                'bool' => [
-                                    'filter' => [
-                                        'term' => [
-                                            'document_type' => ProductInterface::class
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ],
-                $searchAfter ? [
-                    'search_after' => $searchAfter
-                ] : []
-            );
-            $results = $this->productAndProductModelClient->search($params);
-
-            $productsIds = array_map(function ($doc) {
-                return substr($doc['_source']['id'], strlen(ElasticsearchProductProjection::INDEX_PREFIX_ID));
-            }, $results['hits']['hits']);
-
-            yield $productsIds;
-
-            $resultsPage = $results['hits']['hits'];
-            $lastResult = end($resultsPage);
-            $searchAfter = $lastResult['sort'] ?? [];
-        } while (count($resultsPage)>0);
-    }
-
-    private function getAncestorsFromProductsIds(array $productIds): array
-    {
-        $sql = <<<SQL
-            SELECT parent_product_model.code
-            FROM pim_catalog_product product
-                JOIN pim_catalog_product_model product_model
-                    ON product_model.id = product.product_model_id
-                JOIN pim_catalog_product_model parent_product_model
-                    ON parent_product_model.id = product_model.id
-            WHERE product.id IN (:product_ids)
-            UNION DISTINCT
-            SELECT product_model.code
-            FROM pim_catalog_product product
-                JOIN pim_catalog_product_model product_model
-                    ON product_model.id = product.product_model_id
-            WHERE product.id IN (:product_ids)
-        SQL;
-
-        $results = $this->connection->executeQuery(
-            $sql,
-            [
-                'product_ids' => $productIds
-            ],
-            [
-                'product_ids' => Connection::PARAM_STR_ARRAY,
-            ]
-        )->fetchFirstColumn();
-
-        return $results;
+        return $numberOfDeleteDocuments;
     }
 
     private function checkIndexExists(): void